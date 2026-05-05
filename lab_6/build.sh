292bcf846fc43ff7c087b8c2f29155ce95e1e598
Mellmiiii <annagorod-ipt28@lll.kpi.ua>
2026-05-05 23:12:03 +0300

    Revert "Lab 6 (#7)"
    This reverts commit 61818338fc0d4d3a64747d423ff423aae7e8eaee.

--- lab_6/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Building the project..."
-mkdir -p build
-cd build
-cmake ..
-make
-echo "Build complete!"
--- lab_6/preinstall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Installing dependencies..."
-sudo apt update
-sudo apt install -y build-essential cmake libopencv-dev
-echo "Dependencies installed successfully!"
-
